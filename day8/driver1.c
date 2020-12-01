ad88444041dfa263d19daa85f7dc1de26f7f77b4
VinodAntony <antonyvinod@rocketmail.com>
2020-12-01 23:56:59 +0530

    changed code

--- day8/driver1.c
+++ day8/driver1.c
@@ -1,11 +1,24 @@
 #include<linux/module.h>
 #include<linux/kernel.h>
+#include <linux/init.h>
+#include <linux/fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("vinod");
 
+dev_t deviceNumber; 
+
+
 static int __init driver_init(void){
 	
+	int ret;
+	deviceNumber = MKDEV(42,0);
+	ret = register_chrdev_region(deviceNumber,1,"sample_device");
+	if(ret){// IF TRUE, THEN DEVICE NUMBER UNAVAILABLE
+		printk(KERN_ALERT"unsuccessful device registration, kernel : PERMISSION DENIED");
+		return ret;  
+	}
+
 	printk(KERN_ALERT"hello\n");
 	return 0;
 }
